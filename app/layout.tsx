6ea14a332a7c5e038fb82291677df70d23f4017f
dhfai <105841108621@student.unismuh.ac.id>
2024-10-04 02:51:00 +0800

    initial

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* <Toaster /> */}
-
         {children}
       </body>
     </html>
--- components/Peta.tsx
+++ components/Peta.tsx
@@ -1,15 +1,9 @@
 'use client'
 
-import { useState } from 'react'
 import PetaSulsel from './PetaSulsel'
 
 
 export default function Peta() {
-  const [cardActive, setCardActive] = useState(false)
-  const [selectedRegion, setSelectedRegion] = useState(null)
-
-// s
-
   return (
     <div className='flex flex-col justify-center items-center p-6'>
       <h1 className='text-3xl font-bold mb-6'>KABUPATEN SULAWESI SELATAN</h1>
